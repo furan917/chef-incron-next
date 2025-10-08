90b91851810474e26f43310681ec461a11a50f13
Francis Gallagher <francis@akoova.com>
2025-10-08 10:41:46 +0100

    chore: version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Francis Gallagher'
 maintainer_email 'francis@akoova.com'
 license          'MIT'
 description      'Installs and configures incron-next, fork of incron'
-version          '0.3.5'
+version          '0.3.6'
 
 depends 'yum'
 depends 'yum-repoforge'
